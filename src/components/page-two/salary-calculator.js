aaa5bd5d38cc3dfd6ed7adf4d4790062ebb0eda1
sem.gebresilassie@gmail.com <sem.gebresilassie@gmail.com>
2018-07-10 23:30:25 +0300

    Add filtering to calculate single employee salary

--- src/components/page-two/salary-calculator.js
+++ src/components/page-two/salary-calculator.js
@@ -1,31 +1,48 @@
 import React from "react";
 
-export const Calculator = props => {
-  const { employees } = props;
+export class CalculateSalary extends React.Component {
+  state = {
+    employee: []
+  };
   //store employee names from csv, later filter is used to display names only once in the option tags to calculate salary
-  return (
-    <div className="salary_display">
-      <p>
-        Choose employee name from the list below to calculate his/her salary
-      </p>
-      <select>
-        {employees
-          .map(employee => {
-            if (employee["Person ID"]) {
-              return employee["Person Name"];
-            }
-          })
-          .filter((v, i, a) => a.indexOf(v) === i)
-          .map(row => {
-            return (
-              <CalculateSalary name={row} key={Math.random(2)} value={row} />
-            );
-          })}
-      </select>
-    </div>
-  );
-};
+  calculateSalary(e) {
+    const name = e.target.value;
+    const { employees } = this.props;
+    const person = employees.filter(employee => {
+      return employee["Person Name"] === name;
+    });
 
-const CalculateSalary = props => {
-  return <option>{props.name}</option>;
-};
+    this.setState({
+      employee: person
+    });
+  }
+  render() {
+    const { employees } = this.props;
+    return (
+      <div className="salary_display">
+        <p>
+          Choose employee name from the list below to calculate his/her salary
+        </p>
+        <select onChange={this.calculateSalary.bind(this)}>
+          {employees
+            .map(employee => {
+              if (employee["Person ID"]) {
+                return employee["Person Name"];
+              }
+              return "";
+            })
+            .filter((v, i, a) => {
+              return a.indexOf(v) === i;
+            })
+            .map(row => (
+              <Calculater name={row} key={Math.random(2)} value={row} />
+            ))}
+        </select>
+      </div>
+    );
+  }
+}
+
+const Calculater = props => (
+  <option value={props.name["Person ID"]}>{props.name}</option>
+);
