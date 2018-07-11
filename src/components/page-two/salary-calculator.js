f1b8a16d5c9b39aa6cc31d04c9e89e49ddd7cb93
sem.gebresilassie@gmail.com <sem.gebresilassie@gmail.com>
2018-07-11 21:38:43 +0300

    remove "this" binding. use arrow functions instead

--- src/components/page-two/salary-calculator.js
+++ src/components/page-two/salary-calculator.js
@@ -1,11 +1,12 @@
 import React from "react";
+import ReactTable from "react-table";
 
 export class CalculateSalary extends React.Component {
   state = {
     employee: []
   };
   //store employee names from csv, later filter is used to display names only once in the option tags to calculate salary
-  calculateSalary(e) {
+  calculateSalary = e => {
     const name = e.target.value;
     const { employees } = this.props;
     const person = employees.filter(employee => {
@@ -15,7 +16,7 @@ export class CalculateSalary extends React.Component {
     this.setState({
       employee: person
     });
-  }
+  };
   render() {
     const { employees } = this.props;
     return (
@@ -23,7 +24,7 @@ export class CalculateSalary extends React.Component {
         <p>
           Choose employee name from the list below to calculate his/her salary
         </p>
-        <select onChange={this.calculateSalary.bind(this)}>
+        <select onChange={this.calculateSalary}>
           {employees
             .map(employee => {
               if (employee["Person ID"]) {
@@ -34,15 +35,38 @@ export class CalculateSalary extends React.Component {
             .filter((v, i, a) => {
               return a.indexOf(v) === i;
             })
-            .map(row => (
-              <Calculater name={row} key={Math.random(2)} value={row} />
-            ))}
+            .map(row => <DisplayWindow uniqueName={row} />)}
         </select>
+        <ReactTable
+          data={this.state.employee}
+          columns={columns}
+          defaultPageSize={10}
+          style={{
+            height: "400px",
+            width: "50vw"
+          }}
+          className=" -highlight"
+        />
       </div>
     );
   }
 }
-
-const Calculater = props => (
-  <option value={props.name["Person ID"]}>{props.name}</option>
-);
+const columns = [
+  {
+    Header: "Date",
+    accessor: "Date"
+  },
+  {
+    Header: "Shift start",
+    accessor: "Start"
+  },
+  {
+    Header: "Shift end",
+    accessor: "End"
+  }
+];
+const DisplayWindow = props => {
+  return (
+    <option value={props.uniqueName["Person ID"]}>{props.uniqueName}</option>
+  );
+};
