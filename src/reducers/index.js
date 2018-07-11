6ec876ebd12fe40b7c95b9a988750e4408490e58
sem.gebresilassie@gmail.com <sem.gebresilassie@gmail.com>
2018-07-11 21:37:48 +0300

    remove CalculateSalary from reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,8 +5,6 @@ export function globalReducer(state, action) {
       return { ...state, csv: payload };
     case "PROCESS_DATA":
       return { ...state, employees: payload };
-    case "CALCULATE_SALARY":
-      return { ...state, salary: payload };
 
     default:
       return state;
